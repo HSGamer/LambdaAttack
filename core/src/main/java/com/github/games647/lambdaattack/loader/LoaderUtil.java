a54d518a98c3db31e9df5a6671898e5e15e8d121
HSGamer <huynhqtienvtag@gmail.com>
2022-10-13 13:28:56 +0700

    support Main-Class manifest for Bot Creator jar

--- core/src/main/java/com/github/games647/lambdaattack/loader/LoaderUtil.java
+++ core/src/main/java/com/github/games647/lambdaattack/loader/LoaderUtil.java
@@ -6,13 +6,20 @@ import com.github.games647.lambdaattack.bot.BotCreator;
 import java.io.File;
 import java.net.URL;
 import java.net.URLClassLoader;
+import java.util.jar.JarFile;
+import java.util.jar.Manifest;
 import java.util.logging.Level;
 
 public class LoaderUtil {
     public static BotCreator getBotCreator(File file) {
-        try {
+        try (JarFile jarFile = new JarFile(file)){
+            Manifest manifest = jarFile.getManifest();
+            String mainClass = manifest.getMainAttributes().getValue("Main-Class");
+            if (mainClass == null) {
+                mainClass = "com.github.games647.lambdaattack.impl.BotCreatorImpl";
+            }
             URLClassLoader classLoader = new URLClassLoader(new URL[]{file.toURI().toURL()}, LoaderUtil.class.getClassLoader());
-            Class<?> clazz = classLoader.loadClass("com.github.games647.lambdaattack.impl.BotCreatorImpl");
+            Class<?> clazz = classLoader.loadClass(mainClass);
             return (BotCreator) clazz.getDeclaredConstructor().newInstance();
         } catch (Exception e) {
             LambdaAttack.getLogger().log(Level.SEVERE, "Failed to load the file", e);
